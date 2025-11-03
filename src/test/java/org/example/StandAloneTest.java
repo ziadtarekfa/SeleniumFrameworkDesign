a0a9b7c0aa5b4aa9b058a16388431358255083a1
ziadtarekfa <ziadtarekfa@gmail.com>
2025-11-03 15:32:33 +0200

    added product to cart

--- src/test/java/org/example/StandAloneTest.java
+++ src/test/java/org/example/StandAloneTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -14,20 +15,29 @@ import java.util.List;
 public class StandAloneTest {
     public static void main(String[] args) {
 
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-popup-blocking");
+
+        WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://rahulshettyacademy.com/client");
 
 
-        driver.findElement(By.id("userEmail")).sendKeys("anshika@gmail.com");
-        driver.findElement(By.id("userPassword")).sendKeys("Iamking@000");
+        driver.findElement(By.id("userEmail")).sendKeys("ziad@gmail.com");
+        driver.findElement(By.id("userPassword")).sendKeys("Hello123");
         driver.findElement(By.id("login")).click();
 
-        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.alertIsPresent());
-        driver.switchTo().alert().accept();
 
         List <WebElement> products  = driver.findElements(By.className("mb-3"));
+
+
+        WebElement selectedProduct = products.stream().filter(product ->
+                product.findElement(By.tagName("b")).getText().equals("ZARA COAT 3")).findFirst().orElse(null);
+
+        assert selectedProduct != null;
+        selectedProduct.findElement(By.cssSelector(".card-body button:last-of-type")).click();
+
     }
 
 }
