0a0c2398480de5df430143eae6b598b365cf3c69
ziadtarekfa <ziadtarekfa@gmail.com>
2025-11-04 15:22:15 +0200

    added pagefactory structure

--- src/test/java/org/example/StandAloneTest.java
+++ src/test/java/org/example/StandAloneTest.java
@@ -1,11 +1,9 @@
 package org.example;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
